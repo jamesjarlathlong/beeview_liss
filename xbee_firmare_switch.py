a9a779db16a835ba257796ea9ccd7211fd83cb2c
James <jjlong@mit.edu>
2017-05-08 19:43:53 -0400

    added script to update xbee firmamware to api mode 1

---
+++ xbee_firmare_switch.py
@@ -0,0 +1,18 @@
+import serial
+uart = serial.Serial('dev/ttymxc2', 9600)
+ZBee = ZigBee(uart, escaped =False)
+def atap_command():
+	uart.write('+++')
+	waiting = uart.inWaiting()
+	res = uart.read(waiting)
+	if res ==b'OK\r':
+		uart.write('ATAP1\r')
+		waiting = uart.inWaiting()
+		final_res = uart.read(waiting)
+		if final_res ==b'OK\r':
+			print('success')
+			return
+	else:
+		print('oops, try again')
+if __name__ == '__main__':
+	atap_command()
