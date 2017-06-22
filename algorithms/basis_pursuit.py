17c6e9c828219643a9312a5636d042fb9df4563e
James <jjlong@mit.edu>
2017-06-22 16:27:26 -0400

    adding basis pursuit

---
+++ algorithms/basis_pursuit.py
@@ -0,0 +1,83 @@
+"""
+we're trying to solve the problem
+min ||x||_1 st Ax=b where b is the the signal
+A is a basis matrix and x is the solution vector
+"""
+import simplex
+def obj_variables(signal):
+	n = len(signal)
+	return ('x_'+str(i+1) for i in range(2*n))
+def get_coef(n, varname):
+	varnum = varname.split('_')[1]
+	return str(1) if int(varnum) <= n else str(0)
+
+def create_objective_string(the_vars):
+	"""given our signal and our basis matrix
+	return the objective function string 
+	to be fed into the simplex solver
+	string is of the form '4x_1+2_x2...'"""
+	n = len(the_vars)//2
+	with_coef = (get_coef(n,v)+v for v in the_vars)
+	together = (' + ').join(with_coef)
+	return together
+def flatten(gen):
+	return (x for y in gen for x in y)
+def create_l1_constraints(the_vars):
+	"""for every var in the objective function we need
+	to add a constraint for the actual solution var.
+	e.g. s1<=x1, s1>=-t1"""
+	pairs = get_pairs(the_vars)
+	l1_constraints = flatten((get_l1_constraint(pair) for pair in pairs))
+	return list(l1_constraints)
+def get_pairs(the_vars):
+	n = len(the_vars)//2
+	return zip(the_vars[n:], the_vars[0:n])
+def get_l1_constraint(pair):
+	yield diff(stringify(pair))
+	yield summ(stringify(pair))
+def stringify(pair):
+	return [str(1)+p for p in pair]
+def diff(pair):
+	return ' - '.join(pair)+ ' 1 <= 1'
+def summ(pair):
+	return ' + '.join(pair)+ ' 1 >= 1'
+
+def combine_coef_var(tupl):
+	return str(tupl[0])+tupl[1]
+def dotvars(cv_pairs):
+	combined = (combine_coef_var(pair) for pair in cv_pairs)
+	return ' + '.join(combined)
+def zipdot(A, y):
+	return (zip(row, y) for row in A)
+def full_eq(sides):
+	return ' = '.join(sides)
+def create_basis_constraints(signal, A, the_vars):
+	"""given our signal and our basis matrix
+	return the list of constraint strings
+	to be fed into the simplex solver
+	constraint string is of the form '3x_1 + 1x_2 = 3'
+	A is a list of lists(rows)"""
+	n = len(signal)
+	y = the_vars[n:]
+	lhsides = (dotvars(pairs) for pairs in zipdot(A, y))
+	rhsides = (str(i) for i in signal)
+	return [full_eq(two_sides) for two_sides in zip(lhsides, rhsides)]
+def solve_basis_pursuit(A, signal):
+	the_vars = list(obj_variables(signal))
+	obj_fun = create_objective_string(the_vars)
+	l1s = create_l1_constraints(the_vars)
+	equalities = create_basis_constraints(signal, A, the_vars)
+	all_constraints = l1s+equalities
+	Lp_system = simplex.Simplex(num_vars=len(the_vars), constraints=all_constraints, objective_function=('minimize', obj_fun))
+	return Lp_system
+if __name__ == '__main__':
+	signal = [1, 2, 15]
+	A = [[11,12,13]
+		,[21,22,23]
+		,[31,32,33]]
+	solution = solve_basis_pursuit(A, signal)
+	print(Lp_system.solution)
+	print(Lp_system.optimize_val)
+
+
+	
